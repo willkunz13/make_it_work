538dbda2d895d124d96155bc1d64b39c33920543
jerry <jerry@jerrys-MacBook-Pro.local>
2020-02-07 10:54:54 -0700

    Add test for story 4

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,5 @@
 class ProjectsController < ApplicationController
 	def show
 		@project = Project.find(params[:id])
-		binding.pry
 	end
 end
--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -28,4 +28,21 @@ RSpec.describe "On show page" do
 		expect(page).to_not have_content("3")
 		expect(page).to have_content("4")
 	end
+        it "displays average experience of contestants" do
+                challenge = Challenge.create(theme: "nautical", project_budget: 900)
+                project1 = Project.create(name: "dress", material: "fishnet, rope, tarp", challenge_id: challenge.id)
+                project2 = Project.create(name: "raft", material: "carboard, duct-tape", challenge_id: challenge.id)
+                contestant1 = Contestant.create(name: "Fred", age: 20, hometown: "New York", years_of_experience: 1)
+                contestant2 = Contestant.create(name: "Caroll", age: 27, hometown: "Chicago", years_of_experience: 12)
+                contestant3 = Contestant.create(name: "Ladasha", age: 20, hometown: "New York", years_of_experience: 1)
+                project2.contestants << contestant1
+                project2.contestants << contestant2
+                project2.contestants << contestant3
+                visit "/projects/#{project2.id}"
+		expect(page).to have_content("4.67")
+                contestant4 = Contestant.create(name: "Caroll", age: 27, hometown: "Chicago", years_of_experience: 12)
+                project2.contestants << contestant4
+                visit "/projects/#{project2.id}"
+		expect(page).to have_content("6.5")
+        end
 end
